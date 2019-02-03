e59695ab66d5b39e33c4bf82b091dca1fc2c9bdb
Dipak Patil <dipakpatilcse@gmail.com>
2019-02-03 23:19:33 +0530

    Program Update.

---
+++ src/algorithm_pratice/FindSmallestEqualElementInArray.java
@@ -0,0 +1,11 @@
+package algorithm_pratice;
+
+public class FindSmallestEqualElementInArray {
+
+	// From Given 2 Array Find smallest equal Element Array
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/lambda_expression/unit4/ImperativeVsDeclarativeExample1.java
@@ -0,0 +1,17 @@
+package lambda_expression.unit4;
+
+public class ImperativeVsDeclarativeExample1 {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+
+		for (int i = 0; i <= 100; i++) {
+			sum += i;
+		}
+
+		System.out.println("sum :" + sum);
+
+	}
+
+}
