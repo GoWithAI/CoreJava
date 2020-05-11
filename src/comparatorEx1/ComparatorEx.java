685a6b32506b64c3e3797e3e393fff9a581a365e
Dipak Patil <dipakpatilcse@gmail.com>
2020-05-11 10:56:07 +0530

    comparator Example

---
+++ src/comparatorEx1/ComparatorEx.java
@@ -0,0 +1,26 @@
+package comparatorEx1;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ComparatorEx {
+
+	public static void main(String[] args) {
+
+		Comparator<Integer> comparator = new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1.compareTo(o2);
+			}
+		};
+
+		System.out.println("Compare :" + comparator.compare(3, 2));
+
+		Comparator<Integer> comparator2 = (i, i2) -> i.compare(i, i2);
+
+		System.out.println("lambada compare :" + comparator2.compare(3, 2));
+	}
+
+}
