71d29dc11ae8d976d81494b34972974f78a0f191
dipak <dipak@Dipak>
2021-08-16 13:38:18 +0530

    Test

---
+++ src/collections/TreeSetTest.java
@@ -0,0 +1,14 @@
+package collections;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class TreeSetTest {
+
+	public static void main(String[] args) {
+		TreeSet<String> t = new TreeSet();
+		TreeMap<String, String> tmap = new TreeMap<String, String>();
+
+	}
+
+}
